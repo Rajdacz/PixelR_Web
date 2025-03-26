6d5c126e36655991ea7c1d4f8bf4429d66b0771a
Rajdi2 <plrajdiiipl987@gmail.com>
2025-03-26 16:18:32 +0100

    .

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,14 +1,14 @@
 self.addEventListener("install", (event) => {
-	let basePath = self.location.origin;
+	let basePath = self.location.pathname.replace(/serviceWorker.js$/, "");
 
 	event.waitUntil(
 		caches.open("v1").then((cache) => {
 			return cache.addAll([
-				basePath + "/",
-				basePath + "/index.html",
-				basePath + "/style.css",
-				basePath + "/script.js",
-				basePath + "/manifest.json",
+				basePath,
+				basePath + "index.html",
+				basePath + "style.css",
+				basePath + "script.js",
+				basePath + "manifest.json",
 			]);
 		})
 	);
@@ -17,12 +17,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
 	event.respondWith(
 		caches.match(event.request).then((response) => {
-			return (
-				response ||
-				fetch(event.request).catch(() => {
-					console.error("Downloading error:", event.request.url);
-				})
-			);
+			return response || fetch(event.request);
 		})
 	);
 });
