62b209cc228b52b7852262086a5d7ec006b17bd8
Rajdacz <plrajdiiipl987@gmail.com>
2025-03-28 14:31:36 +0100

    update

--- pixelr/canvas.js
+++ pixelr/canvas.js
@@ -2,4 +2,6 @@ export function show(width, height) {
 	const canvas = document.getElementById("canvas");
 	canvas.width = width;
 	canvas.height = height;
+	canvas.parentElement.style.top = `calc(50% - ${height / 2}px)`
+	canvas.parentElement.style.left = `calc(50% - ${width / 2}px)`
 }
--- pixelr/script.js
+++ pixelr/script.js
@@ -2,18 +2,12 @@ import { show } from "./canvas.js";
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
-		let swPath = `${window.location.pathname.replace(
-			/index.html$/,
-			""
-		)}serviceWorker.js`;
+		let swPath = `${window.location.pathname.replace(/index.html$/, "")}serviceWorker.js`;
 
 		navigator.serviceWorker
 			.register(swPath)
 			.then((registration) => {
-				console.log(
-					"Service Worker registered with scope:",
-					registration.scope
-				);
+				console.log("Service Worker registered with scope:", registration.scope);
 			})
 			.catch((error) => {
 				console.log("Service Worker registration failed:", error);
@@ -34,144 +28,119 @@ function toggleMenu(targetId) {
 	});
 }
 
-document
-	.getElementById("file")
-	.addEventListener("click", () => toggleMenu("main-menu"));
-document
-	.getElementById("edit")
-	.addEventListener("click", () => toggleMenu("edit-menu"));
-document
-	.getElementById("help")
-	.addEventListener("click", () => toggleMenu("help-menu"));
+document.getElementById("file").addEventListener("click", () => toggleMenu("main-menu"));
+document.getElementById("edit").addEventListener("click", () => toggleMenu("edit-menu"));
+document.getElementById("help").addEventListener("click", () => toggleMenu("help-menu"));
 document.getElementById("logo").addEventListener("click", () => toggleMenu(""));
 
-// Themes
+// Theme toggle functions
+function setTheme(theme) {
+	const themeProperties = theme === 'dark' ? {
+		bgColor: "var(--dark-bg-primary)",
+		textColor: "var(--dark-text-light)",
+		bgSecondaryColor: "var(--dark-bg-secondary)",
+		textSecondaryColor: "var(--dark-text-secondary)",
+		shadow: "var(--dark-shadow)",
+		buttonColor: "var(--primary-500)",
+		buttonUrl: "var(--dark-button-url)",
+		themeIconSrc: "assets/moon.png"
+	} : {
+		bgColor: "var(--light-bg-primary)",
+		textColor: "var(--light-text-dark)",
+		bgSecondaryColor: "var(--light-bg-secondary)",
+		textSecondaryColor: "var(--light-text-secondary)",
+		shadow: "var(--light-shadow)",
+		buttonColor: "var(--primary-500)",
+		buttonUrl: "var(--light-button-url)",
+		themeIconSrc: "assets/sun.png"
+	};
 
-function setDarkTheme() {
-	document.querySelector("#theme-toggle img").src = "assets/moon.png";
-	document.documentElement.style.setProperty(
-		"--bg-color",
-		"var(--dark-bg-primary)"
-	);
-	document.documentElement.style.setProperty(
-		"--text-color",
-		"var(--dark-text-light)"
-	);
-	document.documentElement.style.setProperty(
-		"--bg-secondary-color",
-		"var(--dark-bg-secondary)"
-	);
-	document.documentElement.style.setProperty(
-		"--text-secondary-color",
-		"var(--dark-text-secondary)"
-	);
-	document.documentElement.style.setProperty("--shadow", "var(--dark-shadow)");
-	document.documentElement.style.setProperty(
-		"--primary-button-color",
-		"var(--primary-500)"
-	);
-	document.documentElement.style.setProperty(
-		"--button-url",
-		"var(--dark-button-url)"
-	);
-}
-
-function setLightTheme() {
-	document.querySelector("#theme-toggle img").src = "assets/sun.png";
-	document.documentElement.style.setProperty(
-		"--bg-color",
-		"var(--light-bg-primary)"
-	);
-	document.documentElement.style.setProperty(
-		"--text-color",
-		"var(--light-text-dark)"
-	);
-	document.documentElement.style.setProperty(
-		"--bg-secondary-color",
-		"var(--light-bg-secondary)"
-	);
-	document.documentElement.style.setProperty(
-		"--text-secondary-color",
-		"var(--light-text-secondary)"
-	);
-	document.documentElement.style.setProperty("--shadow", "var(--light-shadow)");
-	document.documentElement.style.setProperty(
-		"--primary-button-color",
-		"var(--primary-500)"
-	);
-	document.documentElement.style.setProperty(
-		"--button-url",
-		"var(--light-button-url)"
-	);
+	document.documentElement.style.setProperty("--bg-color", themeProperties.bgColor);
+	document.documentElement.style.setProperty("--text-color", themeProperties.textColor);
+	document.documentElement.style.setProperty("--bg-secondary-color", themeProperties.bgSecondaryColor);
+	document.documentElement.style.setProperty("--text-secondary-color", themeProperties.textSecondaryColor);
+	document.documentElement.style.setProperty("--shadow", themeProperties.shadow);
+	document.documentElement.style.setProperty("--primary-button-color", themeProperties.buttonColor);
+	document.documentElement.style.setProperty("--button-url", themeProperties.buttonUrl);
+	document.querySelector("#theme-toggle img").src = themeProperties.themeIconSrc;
 }
 
 function updateThemeIcon() {
-	const themeIcon = document.querySelector("#theme-toggle img");
-	const currentTheme = getComputedStyle(document.documentElement)
-		.getPropertyValue("--bg-color")
-		.trim();
-
-	if (
-		currentTheme ===
-		getComputedStyle(document.documentElement)
-			.getPropertyValue("--dark-bg-primary")
-			.trim()
-	) {
-		themeIcon.src = "assets/moon.png";
-	} else {
-		themeIcon.src = "assets/sun.png";
-	}
+	const currentTheme = getComputedStyle(document.documentElement).getPropertyValue("--bg-color").trim();
+	const isDark = currentTheme === getComputedStyle(document.documentElement).getPropertyValue("--dark-bg-primary").trim();
+	document.querySelector("#theme-toggle img").src = isDark ? "assets/moon.png" : "assets/sun.png";
 }
 
 const themeToggleButton = document.getElementById("theme-toggle");
-
 themeToggleButton.addEventListener("click", () => {
-	const currentTheme = getComputedStyle(document.documentElement)
-		.getPropertyValue("--bg-color")
-		.trim();
-
-	if (
-		currentTheme ===
-		getComputedStyle(document.documentElement)
-			.getPropertyValue("--dark-bg-primary")
-			.trim()
-	) {
-		setLightTheme();
-	} else {
-		setDarkTheme();
-	}
+	const currentTheme = getComputedStyle(document.documentElement).getPropertyValue("--bg-color").trim();
+	setTheme(currentTheme === getComputedStyle(document.documentElement).getPropertyValue("--dark-bg-primary").trim() ? 'light' : 'dark');
+	
+	updateThemeIcon();
 });
 
 updateThemeIcon();
 
-// -- Theme
+// Modal management
+let currentModalName;
 
-function showModal(content) {
-	const modal = document.getElementById("modal");
-
-	modal.firstChild.innerHTML = content;
-
-	modal.classList.add("shown");
-	modal.classList.remove("hidden");
+function showModal(name, content) {
+	document.getElementById("modal-content").innerHTML = content;
+	document.getElementById("modal").classList.add("shown");
+	document.getElementById("modal").classList.remove("hidden");
+	currentModalName = name;
 }
 
 function hideModal() {
 	const modal = document.getElementById("modal");
-
 	modal.classList.add("hidden");
 	modal.classList.remove("shown");
 }
 
-document.getElementById("modal").addEventListener("click", () => {
-	hideModal();
-});
-
-document.getElementById("modal-content").addEventListener("click", (event) => {
-	event.stopPropagation();
-});
+document.getElementById("modal").addEventListener("click", hideModal);
+document.getElementById("modal-content").addEventListener("click", (event) => event.stopPropagation());
 
 document.getElementById("new-project").addEventListener("click", () => {
-	showModal(
-		'<button id="modal-submit" class="button primary-button">OK</button><button id="modal-close" class="button secondary-button">Cencel</button><h3>Project name</h3><input class="input" id="modal-input" />'
+	showModal("new-project",
+		`<button id="modal-submit" class="button primary-button">OK</button>
+		<button id="modal-close" class="button secondary-button">Cancel</button>
+		<p>Project name</p>
+		<input class="input" id="modal-new-project-name" />
+		<p>Size</p>
+		<input class="input" id="modal-new-project-width" type="number" placeholder="Width"/>
+		<input class="input" id="modal-new-project-height" placeholder="Height" type="number"/>`
 	);
+
+	document.getElementById("modal-submit").addEventListener("click", () => submitModal(currentModalName));
+	document.getElementById("modal-close").addEventListener("click", hideModal);
+
+	["modal-new-project-width", "modal-new-project-height"].forEach(id => {
+		document.getElementById(id).addEventListener("input", () => {
+			checkNumberInputRange(document.getElementById(id), 1, 100);
+		});
+	});
+
+	// Initial validation
+	checkNumberInputRange(document.getElementById("modal-new-project-width"), 1, 100);
+	checkNumberInputRange(document.getElementById("modal-new-project-height"), 1, 100);
 });
+
+function checkNumberInputRange(input, min, max) {
+	if (Number(input.value) < min || Number(input.value) > max) {
+		input.style.filter = "brightness(0.7) sepia(1) saturate(100%) hue-rotate(-50deg)";
+		return false
+	} else {
+		input.style.filter = "brightness(1) sepia(0) saturate(0%) hue-rotate(0deg)";
+		return true
+	}
+}
+
+function submitModal(currentModalName) {
+	hideModal();
+	if (currentModalName === "new-project") {
+		console.log("Project created!");
+	}
+}
+
+// Canvas
+show(100, 100)
