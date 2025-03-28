792311f64d65c686918614c7ab03a6cd61f2f99e
Rajdi2 <plrajdiiipl987@gmail.com>
2025-03-28 18:37:13 +0100

    .

--- pixelr/canvas.js
+++ pixelr/canvas.js
@@ -1,7 +1,13 @@
-export function show(width, height) {
+export function canvasShow(width, height) {
 	const canvas = document.getElementById("canvas");
 	canvas.width = width;
 	canvas.height = height;
-	canvas.parentElement.style.top = `calc(50% - ${height / 2}px)`
-	canvas.parentElement.style.left = `calc(50% - ${width / 2}px)`
+	canvas.style.top = `calc(50% - ${height / 2}px)`;
+	canvas.style.left = `calc(50% - ${width / 2}px)`;
+}
+
+export function canvasHide() {
+	const canvas = document.getElementById("canvas");
+	canvas.width = 0;
+	canvas.height = 0;
 }
--- pixelr/script.js
+++ pixelr/script.js
@@ -1,13 +1,19 @@
-import { show } from "./canvas.js";
+import { canvasShow } from "./canvas.js";
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
-		let swPath = `${window.location.pathname.replace(/index.html$/, "")}serviceWorker.js`;
+		let swPath = `${window.location.pathname.replace(
+			/index.html$/,
+			""
+		)}serviceWorker.js`;
 
 		navigator.serviceWorker
 			.register(swPath)
 			.then((registration) => {
-				console.log("Service Worker registered with scope:", registration.scope);
+				console.log(
+					"Service Worker registered with scope:",
+					registration.scope
+				);
 			})
 			.catch((error) => {
 				console.log("Service Worker registration failed:", error);
@@ -28,54 +34,102 @@ function toggleMenu(targetId) {
 	});
 }
 
-document.getElementById("file").addEventListener("click", () => toggleMenu("main-menu"));
-document.getElementById("edit").addEventListener("click", () => toggleMenu("edit-menu"));
-document.getElementById("help").addEventListener("click", () => toggleMenu("help-menu"));
+document
+	.getElementById("file")
+	.addEventListener("click", () => toggleMenu("main-menu"));
+document
+	.getElementById("edit")
+	.addEventListener("click", () => toggleMenu("edit-menu"));
+document
+	.getElementById("help")
+	.addEventListener("click", () => toggleMenu("help-menu"));
 document.getElementById("logo").addEventListener("click", () => toggleMenu(""));
 
 // Theme toggle functions
 function setTheme(theme) {
-	const themeProperties = theme === 'dark' ? {
-		bgColor: "var(--dark-bg-primary)",
-		textColor: "var(--dark-text-light)",
-		bgSecondaryColor: "var(--dark-bg-secondary)",
-		textSecondaryColor: "var(--dark-text-secondary)",
-		shadow: "var(--dark-shadow)",
-		buttonColor: "var(--primary-500)",
-		buttonUrl: "var(--dark-button-url)",
-		themeIconSrc: "assets/moon.png"
-	} : {
-		bgColor: "var(--light-bg-primary)",
-		textColor: "var(--light-text-dark)",
-		bgSecondaryColor: "var(--light-bg-secondary)",
-		textSecondaryColor: "var(--light-text-secondary)",
-		shadow: "var(--light-shadow)",
-		buttonColor: "var(--primary-500)",
-		buttonUrl: "var(--light-button-url)",
-		themeIconSrc: "assets/sun.png"
-	};
+	const themeProperties =
+		theme === "dark"
+			? {
+					bgColor: "var(--dark-bg-primary)",
+					textColor: "var(--dark-text-light)",
+					bgSecondaryColor: "var(--dark-bg-secondary)",
+					textSecondaryColor: "var(--dark-text-secondary)",
+					shadow: "var(--dark-shadow)",
+					buttonColor: "var(--primary-500)",
+					buttonUrl: "var(--dark-button-url)",
+					themeIconSrc: "assets/moon.png",
+			  }
+			: {
+					bgColor: "var(--light-bg-primary)",
+					textColor: "var(--light-text-dark)",
+					bgSecondaryColor: "var(--light-bg-secondary)",
+					textSecondaryColor: "var(--light-text-secondary)",
+					shadow: "var(--light-shadow)",
+					buttonColor: "var(--primary-500)",
+					buttonUrl: "var(--light-button-url)",
+					themeIconSrc: "assets/sun.png",
+			  };
 
-	document.documentElement.style.setProperty("--bg-color", themeProperties.bgColor);
-	document.documentElement.style.setProperty("--text-color", themeProperties.textColor);
-	document.documentElement.style.setProperty("--bg-secondary-color", themeProperties.bgSecondaryColor);
-	document.documentElement.style.setProperty("--text-secondary-color", themeProperties.textSecondaryColor);
-	document.documentElement.style.setProperty("--shadow", themeProperties.shadow);
-	document.documentElement.style.setProperty("--primary-button-color", themeProperties.buttonColor);
-	document.documentElement.style.setProperty("--button-url", themeProperties.buttonUrl);
-	document.querySelector("#theme-toggle img").src = themeProperties.themeIconSrc;
+	document.documentElement.style.setProperty(
+		"--bg-color",
+		themeProperties.bgColor
+	);
+	document.documentElement.style.setProperty(
+		"--text-color",
+		themeProperties.textColor
+	);
+	document.documentElement.style.setProperty(
+		"--bg-secondary-color",
+		themeProperties.bgSecondaryColor
+	);
+	document.documentElement.style.setProperty(
+		"--text-secondary-color",
+		themeProperties.textSecondaryColor
+	);
+	document.documentElement.style.setProperty(
+		"--shadow",
+		themeProperties.shadow
+	);
+	document.documentElement.style.setProperty(
+		"--primary-button-color",
+		themeProperties.buttonColor
+	);
+	document.documentElement.style.setProperty(
+		"--button-url",
+		themeProperties.buttonUrl
+	);
+	document.querySelector("#theme-toggle img").src =
+		themeProperties.themeIconSrc;
 }
 
 function updateThemeIcon() {
-	const currentTheme = getComputedStyle(document.documentElement).getPropertyValue("--bg-color").trim();
-	const isDark = currentTheme === getComputedStyle(document.documentElement).getPropertyValue("--dark-bg-primary").trim();
-	document.querySelector("#theme-toggle img").src = isDark ? "assets/moon.png" : "assets/sun.png";
+	const currentTheme = getComputedStyle(document.documentElement)
+		.getPropertyValue("--bg-color")
+		.trim();
+	const isDark =
+		currentTheme ===
+		getComputedStyle(document.documentElement)
+			.getPropertyValue("--dark-bg-primary")
+			.trim();
+	document.querySelector("#theme-toggle img").src = isDark
+		? "assets/moon.png"
+		: "assets/sun.png";
 }
 
 const themeToggleButton = document.getElementById("theme-toggle");
 themeToggleButton.addEventListener("click", () => {
-	const currentTheme = getComputedStyle(document.documentElement).getPropertyValue("--bg-color").trim();
-	setTheme(currentTheme === getComputedStyle(document.documentElement).getPropertyValue("--dark-bg-primary").trim() ? 'light' : 'dark');
-	
+	const currentTheme = getComputedStyle(document.documentElement)
+		.getPropertyValue("--bg-color")
+		.trim();
+	setTheme(
+		currentTheme ===
+			getComputedStyle(document.documentElement)
+				.getPropertyValue("--dark-bg-primary")
+				.trim()
+			? "light"
+			: "dark"
+	);
+
 	updateThemeIcon();
 });
 
@@ -98,49 +152,156 @@ function hideModal() {
 }
 
 document.getElementById("modal").addEventListener("click", hideModal);
-document.getElementById("modal-content").addEventListener("click", (event) => event.stopPropagation());
+document
+	.getElementById("modal-content")
+	.addEventListener("click", (event) => event.stopPropagation());
 
 document.getElementById("new-project").addEventListener("click", () => {
-	showModal("new-project",
+	showModal(
+		"new-project",
 		`<button id="modal-submit" class="button primary-button">OK</button>
 		<button id="modal-close" class="button secondary-button">Cancel</button>
 		<p>Project name</p>
-		<input class="input" id="modal-new-project-name" />
+		<input class="input" id="modal-new-project-name" maxlength="32" />
 		<p>Size</p>
 		<input class="input" id="modal-new-project-width" type="number" placeholder="Width"/>
 		<input class="input" id="modal-new-project-height" placeholder="Height" type="number"/>`
 	);
 
-	document.getElementById("modal-submit").addEventListener("click", () => submitModal(currentModalName));
+	document
+		.getElementById("modal-submit")
+		.addEventListener("click", () => submitModal(currentModalName));
 	document.getElementById("modal-close").addEventListener("click", hideModal);
 
-	["modal-new-project-width", "modal-new-project-height"].forEach(id => {
+	document
+		.getElementById("modal-new-project-name")
+		.addEventListener("input", () => {
+			checkNumberInputRange(
+				document.getElementById("modal-new-project-name"),
+				1,
+				Infinity
+			);
+		});
+
+	["modal-new-project-width", "modal-new-project-height"].forEach((id) => {
 		document.getElementById(id).addEventListener("input", () => {
-			checkNumberInputRange(document.getElementById(id), 1, 100);
+			checkNumberInputRange(document.getElementById(id), 1, Infinity);
 		});
 	});
-
-	// Initial validation
-	checkNumberInputRange(document.getElementById("modal-new-project-width"), 1, 100);
-	checkNumberInputRange(document.getElementById("modal-new-project-height"), 1, 100);
 });
 
 function checkNumberInputRange(input, min, max) {
 	if (Number(input.value) < min || Number(input.value) > max) {
-		input.style.filter = "brightness(0.7) sepia(1) saturate(100%) hue-rotate(-50deg)";
-		return false
+		input.style.borderImage = 'url("assets/buttons/danger.png") 3 fill';
+		input.style.borderImageWidth = "var(--margin-m)";
+		return false;
 	} else {
-		input.style.filter = "brightness(1) sepia(0) saturate(0%) hue-rotate(0deg)";
-		return true
+		input.style.borderImage = "var(--button-url) 3 fill";
+		input.style.borderImageWidth = "var(--margin-m)";
+		return true;
+	}
+}
+
+function checkTextInputLength(input, min, max) {
+	if (Number(input.value.length) < min || Number(input.value.length) > max) {
+		input.style.borderImage = 'url("assets/buttons/danger.png") 3 fill';
+		input.style.borderImageWidth = "var(--margin-m)";
+		return false;
+	} else {
+		input.style.borderImage = "var(--button-url) 3 fill";
+		input.style.borderImageWidth = "var(--margin-m)";
+		return true;
 	}
 }
 
 function submitModal(currentModalName) {
-	hideModal();
 	if (currentModalName === "new-project") {
-		console.log("Project created!");
+		const isWidthValid = checkNumberInputRange(
+			document.getElementById("modal-new-project-width"),
+			1,
+			Infinity
+		);
+		const isHeightValid = checkNumberInputRange(
+			document.getElementById("modal-new-project-height"),
+			1,
+			Infinity
+		);
+		const isNameValid = checkTextInputLength(
+			document.getElementById("modal-new-project-name"),
+			1,
+			32
+		);
+
+		if (isWidthValid && isHeightValid && isNameValid) {
+			hideModal();
+			canvasShow(
+				document.getElementById("modal-new-project-width").value,
+				document.getElementById("modal-new-project-height").value
+			);
+		}
 	}
 }
 
 // Canvas
-show(100, 100)
+let canvas = document.getElementById("canvas");
+let container = document.getElementById("canvas-container");
+let newX = 0,
+	newY = 0,
+	startX = 0,
+	startY = 0;
+
+/*container.addEventListener("wheel", function (event) {
+	event.preventDefault();
+
+	const rect = canvas.getBoundingClientRect();
+	const mouseX = event.clientX - rect.left;
+	const mouseY = event.clientY - rect.top;
+
+	if (event.deltaY < 0) {
+		scale += 0.1;
+	} else {
+		scale -= 0.1;
+	}
+
+	scale = Math.min(Math.max(scale, 0.1), 3);
+
+	const originX = (mouseX / rect.width) * 100;
+	const originY = (mouseY / rect.height) * 100;
+
+	canvas.style.transformOrigin = `${originX}% ${originY}%`;
+	canvas.style.transform = `scale(${scale}) translate(${offsetX}px, ${offsetY}px)`;
+});*/
+
+function startDrag(event) {
+	startX = event.clientX || event.touches[0].clientX;
+	startY = event.clientY || event.touches[0].clientY;
+
+	document.addEventListener("mousemove", mousemove);
+	document.addEventListener("touchmove", mousemove); // Dodajemy touchmove
+}
+
+function stopDrag() {
+	document.removeEventListener("mousemove", mousemove);
+	document.removeEventListener("touchmove", mousemove); // Dodajemy touchmove
+}
+
+function mousemove(event) {
+	newX = startX - (event.clientX || event.touches[0].clientX);
+	newY = startY - (event.clientY || event.touches[0].clientY);
+
+	startX = event.clientX || event.touches[0].clientX;
+	startY = event.clientY || event.touches[0].clientY;
+
+	canvas.style.top = canvas.offsetTop - newY + "px";
+	canvas.style.left = canvas.offsetLeft - newX + "px";
+}
+
+// Dla urządzeń z myszką
+container.addEventListener("mousedown", startDrag);
+document.addEventListener("mouseup", stopDrag);
+
+// Dla urządzeń dotykowych
+container.addEventListener("touchstart", startDrag);
+document.addEventListener("touchend", stopDrag);
+
+canvasShow(200, 200);
